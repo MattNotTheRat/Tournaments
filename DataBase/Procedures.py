3b29f47ff11badfdf09766ae1d189b07c667edca
MrPasto <kostyakas24@gmail.com>
2023-03-31 16:53:40 +0300

    PythonWithSQL2

--- DataBase/Procedures.py
+++ DataBase/Procedures.py
@@ -1,7 +1,10 @@
 import pyodbc
 
+def Option():
+    return 'DRIVER={SQL Server};SERVER=.;DATABASE=Tournament'
+
 def GetTournamentTimeByTournamentId(Id):
-    connection_string = 'DRIVER={SQL Server};SERVER=.;DATABASE=Tournamets'
+    connection_string = Option()
     connection = pyodbc.connect(connection_string)
     cursor = connection.cursor()
     cursor.execute(f"GetTournamentTimeByTournamentId {Id}")
@@ -11,7 +14,7 @@ def GetTournamentTimeByTournamentId(Id):
 #print(GetTournamentTimeByTournamentId(1))
 
 def GetTournamentMestoByTournamentId(Id):
-    connection_string = 'DRIVER={SQL Server};SERVER=.;DATABASE=Tournamets'
+    connection_string = Option()
     connection = pyodbc.connect(connection_string)
     cursor = connection.cursor()
     cursor.execute(f"GetTournamentMestoByTournamentId {Id}")
@@ -21,7 +24,7 @@ def GetTournamentMestoByTournamentId(Id):
 #print(GetTournamentMestoByTournamentId(1))
 
 def GetAllTournamentsWhereParticipatedTeamByTeamId(Id):
-    connection_string = 'DRIVER={SQL Server};SERVER=.;DATABASE=Tournamets'
+    connection_string = Option()
     connection = pyodbc.connect(connection_string)
     cursor = connection.cursor()
     cursor.execute(f"GetAllTournamentsWhereParticipatedTeamByTeamId {Id}")
@@ -31,7 +34,7 @@ def GetAllTournamentsWhereParticipatedTeamByTeamId(Id):
 #print (GetAllTournamentsWhereParticipatedTeamByTeamId(1))
 
 def GetParticipantsByTeamId(Id):
-    connection_string = 'DRIVER={SQL Server};SERVER=.;DATABASE=Tournamets'
+    connection_string = Option()
     connection = pyodbc.connect(connection_string)
     cursor = connection.cursor()
     cursor.execute(f"GetParticipantsByTeamId {Id}")
@@ -41,7 +44,7 @@ def GetParticipantsByTeamId(Id):
 #print(GetParticipantsByTeamId(1))
 
 def GetTeamNameByTournamentId(Id):
-    connection_string = 'DRIVER={SQL Server};SERVER=.;DATABASE=Tournamets'
+    connection_string = Option()
     connection = pyodbc.connect(connection_string)
     cursor = connection.cursor()
     cursor.execute(f"GetTeamNameByTournamentId {Id}")
@@ -49,3 +52,123 @@ def GetTeamNameByTournamentId(Id):
     print(result)
 
 #print(GetTeamNameByTournamentId(1))
+
+def AddTeamInTournament(IdTeam, IdTournament, Mesto, Time):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"AddTeamInTournament {IdTeam},{IdTournament},{Mesto},{Time}")
+    connection.commit()
+    connection.close()
+
+#AddTeamInTournament(4,4,'СтадионШКИЛЬНИКОВ',1000)
+
+def DeleteTeamFromTournament(TeamId):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"DeleteTeamFromTournament {TeamId}")
+    connection.commit()
+    connection.close()
+
+#DeleteTeamFromTournament(6)
+
+def AddUserInTeam(UserId,TeamId):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"AddUserInTeam {UserId},{TeamId}")
+    connection.commit()
+    connection.close()
+
+#AddUserInTeam(11,6)
+
+def DeleteUserFromTeam(UserId):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"DeleteUserFromTeam {UserId}")
+    connection.commit()
+    connection.close()
+
+#DeleteUserFromTeam(11)
+
+def CreateTournament(TournamentName,DiciplineId,OwnerId):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"CreateTournament {TournamentName},{DiciplineId},{OwnerId}")
+    connection.commit()
+    connection.close()
+
+#CreateTournament('СпартакиадаШкольников',3,1)
+
+def DeleteTournament(TournamentId):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"DeleteTournament {TournamentId}")
+    connection.commit()
+    connection.close()
+
+#DeleteTournament(3)
+
+def CreateUser(UserName):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"CreateUser {UserName}")
+    connection.commit()
+    connection.close()
+
+#CreateUser('Богдан')
+
+def DeleteUser(UserId):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"DeleteUser {UserId}")
+    connection.commit()
+    connection.close()
+
+#DeleteUser(15)
+
+def CreateTeam(TeamName):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"CreateTeam {TeamName}")
+    connection.commit()
+    connection.close()
+
+#CreateTeam('Чемпионы')
+
+def DeleteTeam(TeamId):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"DeleteTeam {TeamId}")
+    connection.commit()
+    connection.close()
+
+#DeleteTeam(6)
+
+def UpdateTournamentMesto(IdTournament,Mesto):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"UpdateTournamentMesto {IdTournament},{Mesto}")
+    connection.commit()
+    connection.close()
+
+#UpdateTournamentMesto(4,'СтадионКудрово')
+
+def UpdateTournamentTime(IdTournament,Time):
+    connection_string = Option()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    cursor.execute(f"UpdateTournamentTime {IdTournament},{Time}")
+    connection.commit()
+    connection.close()
+
+#UpdateTournamentTime(4,1430)
